db2feeb33abcd4d2897c85320bcdc78a966e8e12
Italo Santos <italo.ssantoss17@gmail.com>
2025-07-30 02:18:39 -0300

    ok

--- curso/aula5/ex01.js
+++ curso/aula5/ex01.js
@@ -1,6 +1,6 @@
-var vel = 100.1
+var vel = 20
 console.log(`A velocidade do seu carro é ${vel} km/h`)
-if (vel > 60){
+if (vel > 60){// condição simples!
     console.log (` Você ultrapassou a velocidade permitida.MULTADO!`)
 }
 console.log (`Dirija sempre usando cinto de segurança!`)
